83bbc0f13a3ea441a8404db83182160ef6c8116e
Cam Huynh <huynhquancam@gmail.com>
2016-09-16 23:01:23 +0700

    Update

---
+++ vim/ftdetect/cpp.vim
@@ -0,0 +1,1 @@
+autocmd BufRead,BufNewFile *.cpp set ft=cpp
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,12 +1,12 @@
 # Export paths
-export GOPATH="/Users/huynhquancam/workspace/go"
 export PATH="$PATH:$HOME/.bin:$GOPATH/bin"
 export PATH="$PATH:/usr/local/bin:/usr/local/sbin"
 export PATH="$PATH:/usr/local/lib/node_modules"
 export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin"
-export PATH="$PATH:$HOME/dotfiles/bin/"
+export PATH="$PATH:$HOME/dotfiles/bin"
 export NOTES_PATH="$HOME/Dropbox/Notes"
 export WORQUE_PATH="$NOTES_PATH/Todos"
+export GOPATH="$HOME:/go"
 
 source $(brew --prefix nvm)/nvm.sh
 # Include Python VirtualEnv & VirtualEnvWrapper
