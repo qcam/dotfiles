7a4ff3ec8deee47b88ae3142fd51d5ed3fbcf371
Cẩm Huỳnh <huynhquancam@gmail.com>
2017-03-24 23:55:41 +0100

    More things
    Signed-off-by: Cẩm Huỳnh <huynhquancam@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,24 +1,34 @@
 source $HOME/dotfiles/zsh/functions.zsh
 
-# Git
-alias ga='git add .'
+# Git world
+alias m='gco master'
+alias ga='git add'
 alias gb='git branch'
 alias gbd='git branch -D $1'
 alias gcm='git commit'
 alias gcma='git commit --amend'
+alias gd='git diff'
+alias gdm='gd master'
 alias gst='git st'
 alias gco='git checkout'
 alias gp='git push'
+alias gpm='git push origin master'
 alias gpf='git push -f'
 alias gpl='git pull'
-alias gfn='git finish'
 alias gl='git lg'
+alias gsq='git squash'
+alias gsqm='gsq master'
+alias gplrm='git plr origin master'
+alias gaa='ga -A'
+alias gag='gaa' # typing gag is faster
+
 alias pr='hub pull-request'
-alias rename="gb -m"
+alias rename="gb -m --"
+alias pick='git pick'
 alias wip="gcm -m WIP"
 alias fixup="gcm -m fixup"
 alias fixed="gcm -m fixed"
-alias yolo='ga && gcm -m "$(curl -s whatthecommit.com/index.txt)"'
+alias cist="hub ci-status"
 
 # Docker
 alias drmi='docker rmi $(docker ps -aq --filter dangling=true)'
@@ -31,7 +41,6 @@ alias v='/usr/local/opt/vim/bin/vim'
 # Utils
 alias mkdir='mkdir -p'
 alias tr='tree -L2'
-alias cpkey='cat ~/.ssh/id_rsa.pub | pbcopy'
 alias ll='ls -la'
 alias gh='open https://github.com/huynhquancam'
 alias fb='open https://facebook.com/huynhquancam'
@@ -47,11 +56,10 @@ alias be='bundle exec'
 alias ber='be rake'
 alias rsp='be rspec'
 alias bers='be rails server -p 8080'
-alias rbtag='ctags -f rbtags -R . $(bundle list --paths)'
+alias rbtag='ctags -f tags -R . $(bundle list --paths)'
 alias rb='rbenv'
 alias rbs='rbenv shell'
 alias rbl='rbenv local'
-alias rbrh='rbenv rehash'
 
 # Node - NPM
 alias ni='npm install'
@@ -63,6 +71,9 @@ alias ne='npm-exec'
 alias wp='npm-exec webpack'
 alias wpdev='npm-exec webpack-dev-server'
 
+# SSH
+alias ssh-tun='ssh -N -R'
+
 # Make
 alias mk='make'
 
@@ -71,5 +82,5 @@ alias vimrc='vim ~/dotfiles/vimrc'
 alias nvimrc='vim ~/dotfiles/config/nvim/init.vim'
 alias zshrc='vim ~/dotfiles/zshrc'
 
-alias shrug='echo "¯\_(ツ)_/¯" | pbcopy'
-alias copy-key='cat ~/.ssh/id_rsa.pub | pbcopy'
+alias shrug='echo -n "¯\_(ツ)_/¯" | pbcopy'
+alias key='cat ~/.ssh/id_rsa.pub | pbcopy'
